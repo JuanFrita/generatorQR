c82e0bd767f267dd7b7ded66b33322a3dab6c5de
juanfransanchez1 <juanfran.sanchez@agiliacenter.com>
2025-03-16 16:03:59 +0100

    Add: base test

--- src/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/class/QrGenerator.php';
-
-#Simple PHP script to output codeGenerator results
-
-$codeGenerator = new QrGenerator();
-
-echo $codeGenerator->generate("https://www.qrcode.com/") . "\n";
